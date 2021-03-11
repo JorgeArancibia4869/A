3a4456683d576ccc657532e87b291d130cefecef
Jorge <jarancibia@waypoint.cl>
2021-03-11 17:51:23 -0300

    v0.0.1

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import * as Linking from 'expo-linking';
+
+const linkToBob = (path, param1, param2) => Linking.openURL(
+    Linking.createURL(path, {
+      param1: param1,
+      param2: param2
+    }, 'wptbobpr')
+)
 
 export default function App() {
+
+  const [value, onChangeText] = useState('path2');
+  const [value2, onChangeText2] = useState('Hola');
+  const [value3, onChangeText3] = useState('Chao');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Mock de Alice</Text>
       <StatusBar style="auto" />
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText2(text)}
+      value={value2}
+    />
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText3(text)}
+      value={value3}
+    />
+      {/* <Text>Url creada: </Text> */}
+      <Button title="Open Bob" onPress={ () => linkToBob(value, value2, value3) } />
     </View>
   );
 }
