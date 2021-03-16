e66a323170b1a31721908dac68b34998fb8c6e62
Leonardo Espinoza <espinoza.leonardo@gmail.com>
2021-03-15 23:54:06 -0300

    Generaliza Alice, para que pueda construit cualquier URL, para llamado por deepLinking

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput, Alert } from 'react-native';
 import * as Linking from 'expo-linking';
 
-const linkToBob = async (path, param1, param2) => {
+
+const appsToLink = {
+  ['Bob']: { scheme: 'wptbobpr', path: '/path2', username: 'Hola', token: 'Chao' },
+  ['AppConductor']: { scheme: 'wpt-driver-app', path: '/login', username: 'juanito', token: '1234567890' },
+  clear: { scheme: '', path: '/', username: '', token: '' }
+};
+
+const linkToBob = async (scheme, path, username, token) => {
   console.log('Creando path...')
   /*
   const url = Linking.createURL(path, {
     queryParams: {
-      param1: param1,
-      param2: param2
+      username: username,
+      token: token
     }, 
     scheme: 'wptbobpr'
   });
   */
-  const url = `wptbobpr://${path}?param1=${param1}&param2=${param2}`
+  const url = `${scheme}:/${path}?username=${username}&token=${token}`
 
   console.log('Path: ' + url);
   let isPossibleOpenURL = false;
@@ -24,6 +31,8 @@ const linkToBob = async (path, param1, param2) => {
     if (isPossibleOpenURL) {
       await Linking.openURL(url);
       console.log("openURL resolved");
+    } else {
+      Alert.alert('Advertencia', 'No hay ninguna app registrada con el scheme ' + scheme);
     }
   } catch(e) {
     console.log(isPossibleOpenURL ? "openURL rejection" : 'canOpenURL rejection');
@@ -39,33 +48,74 @@ const linkToBob = async (path, param1, param2) => {
   console.log('Alice link to Bob Finish');
 }
 
+const initialApp = 'Bob';
+
 export default function App() {
+  const [appDesc, setAppDesc] = useState(initialApp);
+  const [schemeValue, onChangeScheme] = useState(appsToLink[appDesc].scheme);
+  const [pathValue, onChangePath] = useState(appsToLink[appDesc].path);
+  const [usernameValue, onChangeUsername] = useState(appsToLink[appDesc].username);
+  const [tokenValue, onChangeToken] = useState(appsToLink[appDesc].token);
 
-  const [value, onChangeText] = useState('path2');
-  const [value2, onChangeText2] = useState('Hola');
-  const [value3, onChangeText3] = useState('Chao');
-
+  const fillStates = (appname) => {
+    const { scheme, path, username, token } = appsToLink[appname];
+    setAppDesc(appname);
+    onChangeScheme(scheme);
+    onChangePath(path);
+    onChangeUsername(username);
+    onChangeToken(token);
+  };
   return (
     <View style={styles.container}>
-      <Text>Mock de Alice</Text>
+      <Text style={{ fontWeight: 'bold' }}>Mock de Alice</Text>
+      <Text />
       <StatusBar style="auto" />
+      <Text>Para llamar a:</Text>
+      <View style={styles.container2}>
+        <Button style={styles.buttonOps} color="#841584" title="Bob" onPress={() => fillStates('Bob')} />
+        <Button style={styles.buttonOps} color="#158415" title="AppConductor" onPress={() => fillStates('AppConductor')} />
+        <Button style={styles.buttonOps} color="#485148" title="Clear" onPress={() => fillStates('clear')} />
+      </View>
+      <View style={styles.container2}>
+      <Text style={styles.labelInputTxt}>Scheme:</Text>
       <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
-    <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText2(text)}
-      value={value2}
-    />
-    <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText3(text)}
-      value={value3}
-    />
-      {/* <Text>Url creada: </Text> */}
-      <Button title="Open Bob" onPress={ () => linkToBob(value, value2, value3) } />
+        style={styles.texts}
+        onChangeText={text => onChangeScheme(text)}
+        value={schemeValue}
+        placeHolder="Scheme" 
+      />
+      </View>
+      <View style={styles.container2}>
+      <Text style={styles.labelInputTxt}>Path:</Text>
+      <TextInput
+        style={styles.texts}
+        onChangeText={text => onChangePath(text)}
+        value={pathValue}
+        placeHolder="Path" 
+      />
+      </View>
+      <View style={styles.container2}>
+      <Text style={styles.labelInputTxt}>Username:</Text>
+      <TextInput
+        style={styles.texts}
+        onChangeText={text => onChangeUsername(text)}
+        value={usernameValue}
+        placeHolder="Username" 
+      />
+      </View>
+      <View style={styles.container2}>
+      <Text style={styles.labelInputTxt}>Token:</Text>
+      <TextInput
+        style={styles.texts}
+        onChangeText={text => onChangeToken(text)}
+        value={tokenValue}
+        placeHolder="Token" 
+      />
+      </View>
+      <Text />
+      <Text>Url creada:</Text>
+      <Text>{schemeValue}:/{pathValue}?username={usernameValue}{'&'}token={tokenValue}</Text>
+      <Button title="Open App" onPress={ () => linkToBob(schemeValue, pathValue, usernameValue, tokenValue) } />
     </View>
   );
 }
@@ -77,4 +127,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    flexDirection: 'row'
+  },
+  buttonOps: {
+    margin: 4, 
+  },
+  texts: { height: 40, width: 150, borderColor: 'gray', borderWidth: 1 },
+  labelInputTxt: {
+    height: 40, 
+    width: 80
+  }
 });
